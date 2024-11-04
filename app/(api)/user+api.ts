ef3273264e360db1bb4750c1a17ae8bf2ca4f9b8
unknown <93060367+achyu-dev@users.noreply.github.com>
2024-11-04 21:49:18 +0530

    added bottom tab

---
+++ app/(api)/user+api.ts
@@ -0,0 +1,13 @@
+import { neon } from "@neondatabase/serverless";
+
+const sql = neon(
+    `${process.env.DATABASE_URL}`,
+);
+
+export async function POST(request: Request) {
+    try {
+         
+    } catch {
+        console.error();
+    }
+}
